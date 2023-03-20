21b5a0ee037c790f35402f1d6467c8398c0c2cba
Manel Jaballah <mjaballah22@gmail.com>
2023-03-20 17:52:25 -0400

    Here is my updated website

--- src/App.js
+++ src/App.js
@@ -34,10 +34,10 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome on the Name that Tune</h1>
+        <h1 className="App-title">Welcome on the Name that Tune Manel</h1>
       </header>
       <div className="App-images">
-        <p>You will have to change the code to run a real game!</p>
+        <p>hello</p>
       </div>
       <div className="App-buttons">
       </div>
